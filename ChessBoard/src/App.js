6bd81f2da523efae1d26c5547a9aa6ff0bf5eed8
Janvi-Ashara <janviashara16@gmail.com>
2024-04-03 11:04:22 +0530

    chessboard

---
+++ ChessBoard/src/App.js
@@ -0,0 +1,90 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="row">
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-first" />
+      <div className="col box2 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-first" />
+      <div className="col box1 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-first" />
+      <div className="col box2 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-first" />
+      <div className="col box1 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-first" />
+      <div className="col box2 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-first" />
+      <div className="col box1 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-first" />
+      <div className="col box2 order-md-first" />
+    </div>
+    <div className="row">
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-last" />
+      <div className="col box1 order-md-last" />
+      <div className="col box2 order-md-first" />
+      <div className="col box1 order-md-first" />
+    </div>
+  </div>
+  );
+}
+
+export default App;
